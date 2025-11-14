3792237dde3857f44551e581ab5c6cb6a7068131
faiyaz alam <faiyaz0399@gmail.com>
2025-11-14 17:13:39 +0530

    Added user

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,174 @@
+import { ApiResponse } from "../../../../Backend/src/utils/apiResponse.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/apiHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
+const generateAccessAndRefreshTokens = async(userid) => {
+	try {
+		const user = await User.findById(userid);
+		const accessToken = user.generateAccessToken();
+		const refreshToken = user.generateRefreshToken();
+
+		user.refreshToken = refreshToken
+		await user.save({validateBeforeSave:false});
+
+		return {accessToken, refreshToken};
+	} catch (error) {
+		throw new ApiError(500, "Something went wrong while generating refresh and access token")
+	}
+}
+
+const registerUser = asyncHandler(async (req, res) => {
+	const {username, fullName, email, password, role} = req.body
+
+	// console.log(req.body);
+	
+	// if(username === "" || fullName==="")
+	if([username,fullName,email,password, role].some((field)=>
+		field?.trim() === "")
+	){
+		throw new ApiError(400,"All fields are required");
+	}
+
+	const userExists = await User.findOne({
+		$or: [{username}, {email}]
+	})
+
+	if(userExists){
+		throw new ApiError(401, "User already exists")
+	}
+
+	// console.log(req.files);
+	
+	const avatarPath = req.files?.avatar?.[0]?.path;
+	// console.log("avatarPath", avatarPath);
+	
+	if(!avatarPath){
+		throw new ApiError(400,"Avatar path is required")
+	}
+
+	const avatar = await uploadOnCloudinary(avatarPath);
+	// console.log("avatar", avatar);
+	
+
+	if(!avatar){
+		throw new ApiError(500, "Failed to upload avatar")
+	}
+
+	const coverImagePath = req.files?.coverImage?.[0].path;
+	if(!coverImagePath){
+		throw new ApiError(400,"Avatar path is required")
+	}
+
+	const coverImage = await uploadOnCloudinary(coverImagePath);
+
+	if(!coverImage){
+		throw new ApiError(500, "Failed to upload avatar")
+	}
+
+	const user = await User.create({
+		username,
+		fullName,
+		role,
+		email,
+		password,
+		avatar:avatar.secure_url,
+		coverImage:coverImage.secure_url
+	})
+
+	console.log('user', user);
+	
+
+	const createdUser = await User.findById(user._id).select("-password -refreshToken")
+	if(!createdUser){
+		throw new ApiError(500, "Something went wrong while registering the user")
+	}
+
+
+	return res
+		.status(201)
+		.json(new ApiResponse(
+			201,
+			createdUser,
+			"user registered successfully"
+		))
+
+
+})
+
+const loginUser = asyncHandler(async(req, res) => {
+
+	const {username, email, password} = req.body
+	
+
+	if([username,email,password].some(field => field?.trim() === '')){
+		throw new ApiError(400, "all fields are required");
+	}
+
+	const user = await User.findOne({
+		$or: [{username}, {email}]
+	})
+
+	if(!user){
+		throw new ApiError(40, "user doesn't exist. please register")
+	}
+
+	const isValidePassword = await user.isCorrectPassword(password);
+	if(!isValidePassword){
+		throw new ApiError(400, "Invalid user credentials");
+	}
+
+	const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id);
+
+	const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+	const options = {
+		httpOnly : true,
+		secure: true
+	}
+
+	return res
+		.status(200)
+		.cookie("accessToken", accessToken, options)
+		.cookie("refreshToken", refreshToken, options)
+		.json(new ApiResponse(
+			200,
+			{
+				user : loggedInUser, accessToken
+			},
+			"User logged In successfully"
+		))
+
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+	const userId = req.user._id;
+
+	await User.findByIdAndUpdate(
+		userId,
+		{
+			$unset: {refreshToken : 1}
+		},
+		{
+			new: true,
+		}
+	)
+
+	const options = {
+		httpOnly: true,
+		secure: true
+	}
+
+	return res
+		.status(200)
+		.clearCookie("accessToken",options)
+		.clearCookie("refreshToken", options)
+		.json(new ApiResponse(
+			200,
+			{},
+			"Logout successfully"
+		))
+
+})
+export {registerUser, loginUser, logoutUser}
---
+++ server/src/models/user.model.js
@@ -0,0 +1,104 @@
+import mongoose, { Schema } from "mongoose"; 
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		index: true
+	},
+	fullName: {
+		type: String,
+		lowercase:true,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	avatar: {
+		type: String, 
+	},
+	coverImage: {
+		type:String,
+	},
+	password : {
+		type: String,
+		minlength:8,
+		maxlength:20,
+    	required: true,
+	},
+	role: {
+		type: String,
+		enum:['user', 'admin'],
+		default:"user"
+	},
+	watchHistory: [
+		{
+			video : {type:Schema.Types.ObjectId, ref:"Video"},
+			watchedAt: {type: Date, default:Date.now}
+		}
+	],
+	refreshToken: {
+		type: String,
+	},
+	addresses : [
+		{
+			type: Schema.Types.ObjectId,
+			ref:"Address"
+		}
+	]
+	
+}, {timestamps: true})
+
+//create index
+// userSchema.index({name:1});
+
+//hash password before saving
+userSchema.pre("save", async function(next){
+	if(!this.isModified("password")) return next();
+	this.password = await bcrypt.hash(this.password, 10);
+	next();
+})
+
+//compare password
+userSchema.methods.isCorrectPassword = async function(password) {
+	return await bcrypt.compare(password,this.password);
+}
+
+//generate access token
+userSchema.methods.generateAccessToken = function () {
+	return jwt.sign(
+		{
+			_id : this._id,
+			username: this.username,
+			email:this.email,
+			fullName:this.fullName,
+			role:this.role
+		},
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+		}
+	)
+}
+
+userSchema.methods.generateRefreshToken = function () {
+	return jwt.sign(
+		{
+			_id : this._id,
+		},
+		process.env.REFRESH_TOKEN_SECRET,
+		{
+			expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+		}
+	)
+}
+
+export const User = mongoose.model("User", userSchema);
