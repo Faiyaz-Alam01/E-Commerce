5b6afdd2e60eb6b42992bd55edf63d91be95d3a1
faiyaz alam <faiyaz0399@gmail.com>
2025-11-14 17:11:03 +0530

    Added User route

---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { loginUser, registerUser,logoutUser } from "../controllers/user.controller.js";
+import { upload } from "../middleware/multer.js";
+import {verifyJWT} from "../middleware/auth.middleware.js"
+
+const router = Router();
+
+router.route('/register').post(upload.fields([
+	{
+		name:"avatar",
+		maxCount:1
+	},
+	{
+		name:'coverImage',
+		maxCount:1
+	}
+]),	registerUser)
+
+router.route('/login').post(loginUser);
+router.route('/logout').post(verifyJWT,logoutUser)
+
+export default router;
