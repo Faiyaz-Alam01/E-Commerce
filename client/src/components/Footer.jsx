0e0f17284cde2d36be456cce1b484aaed856e0d1
faiyaz alam <faiyaz0399@gmail.com>
2025-11-19 19:04:25 +0530

    Added footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<div className="bg-gray-800 py-4 text-center text-gray-300 border-t">
+		© {new Date().getFullYear()} Faiyaz. All rights reserved.
+		</div>
+
+	)
+}
+
+export default Footer
