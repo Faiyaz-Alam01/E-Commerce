805bde533952652f8d5111c1f963a188f83d49f0
faiyaz alam <faiyaz0399@gmail.com>
2025-12-09 12:22:57 +0530

    Added stores

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import productReducer from "./slices/productSlice";
 
-export default configureStore({
-	reducer:{
-		
-	}
-})
+export const store = configureStore({
+    reducer: {
+        user : authReducer,
+        product: productReducer,
+    },
+});
