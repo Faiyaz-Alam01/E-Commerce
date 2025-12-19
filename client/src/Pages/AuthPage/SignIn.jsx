002693c25c55b2e7a6da57e7dc4e09f78d337d84
faiyaz alam <faiyaz0399@gmail.com>
2025-12-20 01:53:04 +0530

    Added signIn

--- client/src/Pages/AuthPage/SignIn.jsx
+++ client/src/Pages/AuthPage/SignIn.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { userLogin } from '@/redux/slices/authSlice';
+import React, { use, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
 	const[userInput, setUserInput] = useState({
 		email:"",
 		password:""
 	});
-
-	console.log(userInput);
 	
 
 	function inputValues (e) {		
@@ -19,10 +22,42 @@ const SignIn = () => {
 		}))		
 	}
 
+	const handleSubmit = async(e) => {		
+		e.preventDefault();
+
+		if(userInput.email==='' || userInput.password===''){
+			alert("All fields are required");
+			return;	
+		}
+
+		if(userInput.password.length < 6){
+			alert("Password must be at least 6 characters long");
+			return;
+		}
+
+		if(!/\S+@\S+\.\S+/.test(userInput.email)){
+			alert("Please enter a valid email address");
+			return;	
+		}
+
+		const response = await dispatch(userLogin(userInput));
+		if(response?.payload?.success){	
+			navigate('/');
+			setUserInput({
+				email:"",
+				password:""
+			});
+		}
+	}
+
+
 
 	return (
 		<div className='flex justify-center items-center h-screen'>
-			<form className='border p-8 space-y-2'>
+			<form 
+				noValidate
+				onSubmit={handleSubmit}
+				className='border p-8 space-y-2'>
 				<div className='flex flex-col'>
 					<label htmlFor="email" className='font-medium'>Email</label>
 					<input 
@@ -32,7 +67,6 @@ const SignIn = () => {
 						className='border px-2 py-1 w-72 rounded-sm'
 						onChange={inputValues}
 						value={userInput.email}
-						required
 					/>
 				</div>
 				<div className='flex flex-col '>
@@ -44,7 +78,6 @@ const SignIn = () => {
 						className='border px-2 py-1 w-72 rounded-sm'
 						onChange={inputValues}
 						value={userInput.password}
-						required
 					/>
 				</div>
 				<button type='submit' className='bg-blue-500 hover:bg-blue-600 font-medium w-full rounded-sm py-1 mt-4'>
