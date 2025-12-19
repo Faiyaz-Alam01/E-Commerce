cadd296c62cff654425bf42a42ef9e3e747f1c91
faiyaz alam <faiyaz0399@gmail.com>
2025-12-20 01:52:45 +0530

    Chnaged in the logout function

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { ApiResponse } from "../../../../Backend/src/utils/apiResponse.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/apiHandler.js";
@@ -20,17 +20,18 @@ const generateAccessAndRefreshTokens = async(userid) => {
 }
 
 const registerUser = asyncHandler(async (req, res) => {
-	const {username, fullName, email, password, role} = req.body
+	const {username, email, password, role} = req.body
 
-	// console.log(req.body);
-	
-	// if(username === "" || fullName==="")
-	if([username,fullName,email,password, role].some((field)=>
+	if([username,email,password, role].some((field)=>
 		field?.trim() === "")
 	){
 		throw new ApiError(400,"All fields are required");
 	}
 
+	if(password.length < 6){
+		throw new ApiError(400, "Password must be at least 6 characters long")
+	}	
+	
 	const userExists = await User.findOne({
 		$or: [{username}, {email}]
 	})
@@ -41,50 +42,40 @@ const registerUser = asyncHandler(async (req, res) => {
 
 	// console.log(req.files);
 	
-	const avatarPath = req.files?.avatar?.[0]?.path;
+	// const avatarPath = req.files?.avatar?.[0]?.path;
 	// console.log("avatarPath", avatarPath);
 	
-	if(!avatarPath){
-		throw new ApiError(400,"Avatar path is required")
-	}
+	// if(!avatarPath){
+	// 	throw new ApiError(400,"Avatar path is required")
+	// }
 
-	const avatar = await uploadOnCloudinary(avatarPath);
+	// const avatar = await uploadOnCloudinary(avatarPath);
 	// console.log("avatar", avatar);
 	
 
-	if(!avatar){
-		throw new ApiError(500, "Failed to upload avatar")
-	}
+	// if(!avatar){
+	// 	throw new ApiError(500, "Failed to upload avatar")
+	// }
 
-	const coverImagePath = req.files?.coverImage?.[0].path;
-	if(!coverImagePath){
-		throw new ApiError(400,"Avatar path is required")
-	}
+	// const coverImage = await uploadOnCloudinary(coverImagePath);
 
-	const coverImage = await uploadOnCloudinary(coverImagePath);
-
-	if(!coverImage){
-		throw new ApiError(500, "Failed to upload avatar")
-	}
+	// if(!coverImage){
+	// 	throw new ApiError(500, "Failed to upload avatar")
+	// }
 
 	const user = await User.create({
 		username,
-		fullName,
 		role,
 		email,
 		password,
-		avatar:avatar.secure_url,
-		coverImage:coverImage.secure_url
+		// avatar:avatar.secure_url,
 	})
-
-	console.log('user', user);
 	
 
 	const createdUser = await User.findById(user._id).select("-password -refreshToken")
 	if(!createdUser){
 		throw new ApiError(500, "Something went wrong while registering the user")
 	}
-
 
 	return res
 		.status(201)
@@ -98,17 +89,13 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const loginUser = asyncHandler(async(req, res) => {
+	const {email, password} = req.body
 
-	const {username, email, password} = req.body
-	
-
-	if([username,email,password].some(field => field?.trim() === '')){
+	if([email,password].some(field => field?.trim() === '')){
 		throw new ApiError(400, "all fields are required");
 	}
 
-	const user = await User.findOne({
-		$or: [{username}, {email}]
-	})
+	const user = await User.findOne({email})
 
 	if(!user){
 		throw new ApiError(40, "user doesn't exist. please register")
@@ -144,7 +131,7 @@ const loginUser = asyncHandler(async(req, res) => {
 
 const logoutUser = asyncHandler(async(req, res) => {
 	const userId = req.user._id;
-
+	
 	await User.findByIdAndUpdate(
 		userId,
 		{
@@ -157,7 +144,9 @@ const logoutUser = asyncHandler(async(req, res) => {
 
 	const options = {
 		httpOnly: true,
-		secure: true
+		// secure: true
+		secure: process.env.NODE_ENV === "production"
+
 	}
 
 	return res
