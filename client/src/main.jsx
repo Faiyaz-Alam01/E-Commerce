51276c0ed8a7e8e66783512aa0e5048836af8353
faiyaz alam <faiyaz0399@gmail.com>
2025-12-21 08:03:07 +0530

    Implement Redux Persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,17 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import {persistor, store} from './redux/store.js'
+import { Toaster } from 'react-hot-toast'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <Toaster position='top-center'/>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from './reducers'
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/productSlice";
 import categorySlice from "./slices/categoriesSlice";
+import persistReducer from 'redux-persist/es/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import sessionStorage from 'redux-persist/es/storage/session';
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    product: productReducer,
+    category: categorySlice,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage: sessionStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);;
+
 
 export const store = configureStore({
-    reducer: {
-        user : authReducer,
-        product: productReducer,
-        category: categorySlice,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }), 
+
 });
+
+export const persistor = persistStore(store);
