fc0663b4e2b93ce3cacc3740de64fdc6192209cc
faiyaz alam <faiyaz0399@gmail.com>
2026-02-05 23:37:25 +0530

    category route

--- server/src/routes/category.routes.js
+++ server/src/routes/category.routes.js
@@ -3,8 +3,10 @@ import { addcategory, deleteCategory, getAllCategory, showCategory, updateCatego
 
 const router = Router();
 
+
+
 router.route('/add').post(addcategory);
-router.route('/getAll').get(getAllCategory);
+router.route('/all-category').get(getAllCategory);
 router.route('/update/:categoryId').put(updateCategory);
 router.route('/get/:categoryId').get(showCategory);
 router.route('/delete/:categoryId').delete(deleteCategory);
