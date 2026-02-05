da9fcab92133276b0fafc2ce20fa9fc3fd0f6640
faiyaz alam <faiyaz0399@gmail.com>
2026-02-05 23:35:16 +0530

    git ignore

--- server/src/routes/order.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router = Router();
-
-router.get("/product", (req, res) => {
-		res.json({ message: "Product order route" });
-	
-})
-
-export default router;
