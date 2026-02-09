9bbb04d35c6d0a1e0d697835b2013ff342f86939
faiyaz alam <faiyaz0399@gmail.com>
2026-02-09 12:34:23 +0530

    Added coupon

---
+++ server/src/models/coupon.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const couponSchema = new Schema({
+	code : {
+		type: String,
+		required: true,
+		unique: true,
+		uppercase: true,
+		trim:true
+	},
+	discount: {
+		type: String,
+		enum:["percent", 'flat'],
+		required: true,
+	},
+	discountValue:{
+		type: Number,
+		required: true,
+	},
+	minAmount: {
+		type: Number,
+		default: 0
+	},
+	expiryDate: {
+		type: Date,
+		required: true,
+	},
+	isActive:{
+		type: Boolean,
+		default: true
+	},
+
+},{timestamps: true})
+
+const Coupon = mongoose.model("Coupon", couponSchema)
+export default Coupon
