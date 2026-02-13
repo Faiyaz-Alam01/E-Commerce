740c14b10c0557a2dbfa8135ed3ed270a7263152
faiyaz alam <faiyaz0399@gmail.com>
2026-02-13 23:51:52 +0530

    Added cart slice

--- client/src/redux/slices/cartSlice.js
+++ client/src/redux/slices/cartSlice.js
@@ -1,9 +1,67 @@
-const { createSlice } = require("@reduxjs/toolkit")
+import axiosInstance from "@/helper/axiousInstance"
+import toast from "react-hot-toast"
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 
 const initialState = {
-	cart:[],
+	cart:null,
 }
 
+export const addToCart = createAsyncThunk('/cart/add', async({productId, quantity}) =>  {
+	try {
+		const res = axiosInstance.post('/cart',{productId, quantity})
+		toast.promise(res, {
+			loading: "adding to cart...",
+			success: (data) =>{
+				return data?.data?.message || "successfully added to cart"
+			},
+			error: "error adding to cart"
+		})
+
+		return (await res).data
+	} catch (error) {
+		toast.error(error?.response?.data?.message || "product added to cart failed.");
+		throw error;
+	}
+})
+
+export const getCart = createAsyncThunk('/get/items', async() => {
+	try {
+		const res = axiosInstance.get('/cart')
+		toast.promise(res, {
+			loading: 'fetching cart data ...',
+			success : (data)=>{
+				return data?.data?.message || "successfully fetched data"
+			},
+			error: "failed fetch data"
+		})
+
+		return (await res).data;
+
+	} catch (error) {
+		toast.error(error?.response?.data?.message || "product fetched failed.");
+		throw error;
+	}
+})
+
+export const removeItem =createAsyncThunk('/remove/item', async(productId) => {	
+	try {
+		const res = axiosInstance.delete(`/cart/${productId}`);
+		toast.promise(res, {
+			loading:"removing items...",
+			success:(data)=>{
+				return (data?.data?.message || "removed successfully")
+			},
+			error: "failed to remove data"
+		})
+
+		return (await res).data;
+	} catch (error) {
+		console.log(error);
+		
+		toast.error(error?.response?.data?.message || 'failed to remove items')
+	}
+})
 
 const cartSlice = createSlice({
 	name:"cart",
@@ -11,7 +69,10 @@ const cartSlice = createSlice({
 	reducers:{},
 	extraReducers:(builder) => {
 		builder
-			.addCase()
+			.addCase(getCart.fulfilled, (state, action) => {
+				// console.log(action.payload);
+				state.cart = action.payload.data;
+			})
 	}
 })
 
